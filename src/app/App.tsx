0e4a7310f9759660ba1323e5bd18278754571448
rin <836267074@qq.com>
2026-05-20 20:56:02 +0800

    fix: add SidebarLayout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,5 @@
 import { ChatPage } from "../pages/ChatPage";
-import { SidebarLayout } from "./SidebarLayout";
 
 export function App() {
-  return (
-    <>
-      <SidebarLayout />
-      <ChatPage />
-    </>
-  );
+  return <ChatPage />;
 }
